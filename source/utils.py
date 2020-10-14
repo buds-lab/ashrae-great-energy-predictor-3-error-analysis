7e97c36f98b83eab9b242fe88bd0f915001ec7a9
Pony Biam! <43451598+ponybiam@users.noreply.github.com>
2020-10-14 15:15:53 -0300

    update utils

--- source/utils.py
+++ source/utils.py
@@ -58,7 +58,7 @@ def RMSLE(y_real, y_pred):
 
 
 # Function to complete dates
-def completeData(df, start_date, end_date):
+def complete_data(df, start_date, end_date):
 
     """This function reindex a df to have full dates between the period detailed
 
@@ -72,7 +72,7 @@ def completeData(df, start_date, end_date):
     # List of buildings
     bdgs = list(df.building_id.unique())
     # List of dates
-    idx = pd.date_range(start='2017-01-01', end='2018-12-31')
+    idx = pd.date_range(start=start_date, end=end_date)
     dates_df = pd.DataFrame({"timestamp":idx})
 
     dfs = []
@@ -98,8 +98,8 @@ def completeData(df, start_date, end_date):
     return df
 
 
-# Function to plot heatmaps
-def plotHeatmap(group,df,cols):
+# Function to plot heatmaps grouped by 1 feature.
+def plot_heatmap_1group(group,df,cols):
 
     """This function plots data in the df, one subplot per element in "group". Distributed in "cols" columns
 
@@ -142,7 +142,7 @@ def plotHeatmap(group,df,cols):
         ax = axes[i]
         data = np.array(pivot_df)
         #print(f"data shape: {data.shape}")
-        cmap = plt.get_cmap('YlOrRd')
+        cmap = plt.get_cmap('gist_rainbow') #plt.get_cmap('YlOrRd')
         qmesh = ax.pcolormesh(x, y, data, cmap=cmap, rasterized=True, vmin=0, vmax=1)
         # Axis
         ax.axis('tight') 
@@ -162,3 +162,87 @@ def plotHeatmap(group,df,cols):
     plt.subplots_adjust(bottom=0.12)
 
     return fig
+
+
+# Function to plot heatmaps grouped by 1 feature.
+def plot_heatmaps_2group(meter,group1,group2,df,cols):
+
+    """This function plots data in the "df", one plot per element in "group1", with one subplot per element in "group2". Distributed in "cols" columns.
+
+    meter: metername, defined globally\n
+    df: input dataframe with "timestamp", "buiding_id", "rmsle_scaled" and group features\n
+    group1: feature to divide in plots\n
+    group2: feature to divide each plot in subplots\n
+    cols: number of columns to distribute subplots\n
+
+    Return list of figures.
+    """
+    figs_names = [] # here will be saved the id of figure
+    figs = [] # here will be saved the figures
+
+    group1_list = list(df[group1].unique()) # site
+
+    for group in group1_list:
+        
+        figs_names.append(group)
+        # Filter group1 data
+        df1 = df[df[group1] == group]
+        # Group2
+        group2_list = list(df1[group2].unique()) # primary use
+        plots = len(group2_list)      
+        rows = int(plots)
+
+        # Ratios to make height of subplot dependant of number of buldings
+        ratios = [len(df1[df1[group2] == x].building_id.unique()) for x in group2_list]
+
+        # Size of figure dependant on number of plots
+        size = (15,5) if plots == 1 else (15,20)
+
+        fig, axes = plt.subplots(rows, cols, sharex = True, sharey=False, figsize=size, gridspec_kw = {"height_ratios": ratios})
+        axes = axes.flatten() if plots > 1 else axes
+
+        for i,(j,k) in enumerate(zip(group2_list,ratios)):
+
+            # Filter data
+            df_grouped = df1[df1[group2] == j]
+
+            # Pivot data
+            pivot_df = df_grouped.pivot(columns="timestamp", index="building_id", values="rmsle_scaled")
+
+            # Sort in descending order (sum RMSLE)
+            pivot_df["sum"] = np.sum(pivot_df,axis=1).tolist()
+            pivot_df = pivot_df.sort_values("sum")
+            pivot_df.drop("sum",axis=1, inplace=True)
+
+            # Get the data
+            y = np.linspace(0, len(pivot_df), len(pivot_df)+1)
+            x = mdates.drange(pivot_df.columns[0], pivot_df.columns[-1] + dt.timedelta(days=2), dt.timedelta(days=1))
+
+            # Plot
+            ax = axes[i] if plots > 1 else axes
+            data = np.array(pivot_df)
+            cmap = plt.get_cmap('YlOrRd') #plt.get_cmap('gist_rainbow') 
+            qmesh = ax.pcolormesh(x, y, data, cmap=cmap, rasterized=True, vmin=0, vmax=1)
+            # Axis
+            ax.axis('tight') 
+            ax.xaxis_date() # Set up as dates
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%y')) # set date's format
+            yticks = ticker.MaxNLocator(k)
+            ax.yaxis.set_major_locator(yticks)
+            ax.set_yticklabels([]) # omit building ID on y axis
+            ax.set_title(f"{j}", fontdict={'fontsize':10})
+
+        # Color bar  
+        bottom = 0.015 if plots == 1 else 0.09
+        height = 0.01 if plots == 1 else 0.0025
+        cbaxes = fig.add_axes([0.25, bottom, 0.5, height])
+        cbar = fig.colorbar(qmesh, ax=ax, orientation='horizontal', cax = cbaxes)
+        cbar.set_label('Min-Max scaled RMSLE')
+
+        fig.suptitle(f"{meter} meter - Site {group}", y = 1.015, fontsize=16)
+        plt.tight_layout()
+        plt.subplots_adjust(bottom=0.12)
+
+        figs.append(fig)
+
+    return figs_names, figs
